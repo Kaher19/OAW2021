f6e79ce120bfbd608185c6a6c779216b60abcb24
Zeus Sarmiento <zeusjonass@gmail.com>
2021-04-24 11:11:04 -0500

    Funcionalidad agregada selector de fecha

--- assets/js/filtro.js
+++ assets/js/filtro.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-    $('#lista').tablesorter(); 
     $("#filtro").on("keyup", function() {
         var value = $(this).val().toLowerCase();
         $("#lista tbody tr").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
         });
     });
 });
+function filterByDate(año, mes, dia){
+    if(mes<10) mes = "0"+mes;
+    if(dia<10) dia = "0"+dia;
+    fecha = año+"-"+mes+"-"+dia;
+    $("#lista tbody tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(fecha) > -1);
+    });
+}
