2fc3085703b2e2ba2259e2a9a2f27dadde818335
Zeus Sarmiento <zeusjonass@gmail.com>
2021-05-15 18:07:39 -0500

    OptimizacionCliente

--- assets/js/filtro.js
+++ assets/js/filtro.js
@@ -1,16 +1,1 @@
-$(document).ready(function(){
-    $("#filtro").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#lista tbody tr").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-        });
-    });
-});
-function filterByDate(año, mes, dia){
-    if(mes<10) mes = "0"+mes;
-    if(dia<10) dia = "0"+dia;
-    fecha = año+"-"+mes+"-"+dia;
-    $("#lista tbody tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(fecha) > -1);
-    });
-}
+$(document).ready(function(){$("#filtro").on("keyup",function(){var c=$(this).val().toLowerCase();$("#lista tbody tr").filter(function(){$(this).toggle(-1<$(this).text().toLowerCase().indexOf(c))})})});function filterByDate(c,a,b){10>a&&(a="0"+a);10>b&&(b="0"+b);fecha=c+"-"+a+"-"+b;$("#lista tbody tr").filter(function(){$(this).toggle(-1<$(this).text().toLowerCase().indexOf(fecha))})};
